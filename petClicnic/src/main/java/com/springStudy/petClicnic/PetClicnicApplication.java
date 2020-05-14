4f20fb109976d75326dfb7369190dc55cb036bf4
isgm255 <khinthet@isgm2.com>
2020-05-14 08:35:54 +0630

    initialize repo

---
+++ petClicnic/src/main/java/com/springStudy/petClicnic/PetClicnicApplication.java
@@ -0,0 +1,13 @@
+package com.springStudy.petClicnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClicnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClicnicApplication.class, args);
+	}
+
+}
---
+++ petClicnic/src/test/java/com/springStudy/petClicnic/PetClicnicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springStudy.petClicnic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClicnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
